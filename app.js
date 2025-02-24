40b2205265c54d47d28c83ef3390f0a531d741da
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-02-24 15:48:57 +0100

    feat: add moviesRouter, in app.js import cors and use cors, static folder and express.json

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const cors = require("cors");
+const moviesRouter = require("./routers/moviesRouter");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FE_URL,
+  })
+);
+
+app.use("/movies", moviesRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
